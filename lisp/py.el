3e854bfb9daddc0a7768ef94814a7b697323890b
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2022-08-19 13:09:07 +0900

    add py

---
+++ lisp/py.el
@@ -0,0 +1,26 @@
+;;; py.el --- Personal python settings.
+;;; Commentary:
+;;; Code:
+
+(defun python-hook ()
+  "PYTHON MODE HOOK"
+  (highlight-indentation-mode -1)
+  (setq mode-name "🐍"))
+
+(use-package elpy
+  :ensure t
+  :config
+  (elpy-enable)
+  (setq elpy-rpc-timeout nil)
+  :init
+  (add-hook 'elpy-mode-hook #'python-hook))
+
+(with-eval-after-load 'flycheck
+  (setq flycheck-python-flake8-executable "/usr/bin/env python3")
+  (setq flycheck-flake8rc "~/.config/flake8"))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((python . t)))
+
+(provide 'py)
+;;; py.el ends here
