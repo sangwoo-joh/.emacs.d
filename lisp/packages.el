204891c61e8940e4bdc01e65f9778b42f7bc8979
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2022-09-05 14:50:27 +0900

    change tab format

--- lisp/packages.el
+++ lisp/packages.el
@@ -141,15 +141,15 @@
   (setq switch-window-qwerty-shortcuts
 	      '("a" "s" "d" "f" "j" "k" "l" ";" "q" "w" "e" "r" "i" "o" "z" "x" "c" "v"))
   :bind (("C-x o" . switch-window)
-	       ("C-x 1" . switch-window-then-maximize)
-	       ("C-x 2" . switch-window-then-split-below)
-	       ("C-x 3" . switch-window-then-split-right)
-	       ("C-x 0" . switch-window-then-delete)
-	       ("C-x 4 d" . switch-window-then-dired)
-	       ("C-x 4 f" . switch-window-then-find-file)
-	       ;; ("C-x 4 m" . switch-window-then-compose-mail)
-	       ;; ("C-x 4 r" . switch-window-then-find-file-read-only)
-	       ("C-x 4 0" . switch-window-then-kill-buffer)))
+         ("C-x 1" . switch-window-then-maximize)
+         ("C-x 2" . switch-window-then-split-below)
+         ("C-x 3" . switch-window-then-split-right)
+         ("C-x 0" . switch-window-then-delete)
+         ("C-x 4 d" . switch-window-then-dired)
+         ("C-x 4 f" . switch-window-then-find-file)
+         ;; ("C-x 4 m" . switch-window-then-compose-mail)
+         ;; ("C-x 4 r" . switch-window-then-find-file-read-only)
+         ("C-x 4 0" . switch-window-then-kill-buffer)))
 
 (use-package eyebrowse
   :ensure t
